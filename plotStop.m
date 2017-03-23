5d83c2a23802d835c3d2de3dcc386bf69bdf7343
YFSAO <syfshaoyifei@gmail.com>
2017-03-23 17:11:16 +0800

    Update plotStop.m

--- plotStop.m
+++ plotStop.m
@@ -1,5 +1,7 @@
-[s, lat, lon] = textread('project\1', '%s %f %f');
+% 读取数据
+[s, lat, lon] = textread(filename, '%s %f %f');
 
+% 将经纬度坐标转化为直角坐标
 latlon20=[lat, lon];
 axesm utm;
 Z=utmzone(latlon20);
@@ -11,9 +13,11 @@ for i=1:length(latlon20)
      R(i,:)=[x;y];
 end
 
+% 打印图片
 plot(R(:,1),R(:,2),'r*', 'markersize', 2);
 
-% figure(gcf);
-% dcm_obj = datacursormode(gcf);
-% set(dcm_obj,'DisplayStyle','datatip','SnapToDataVertex','off','Enable','on');
-% set(dcm_obj,'UpdateFcn',{@dcmcallback,stationName});
+% 更改图片坐标显示
+figure(gcf);
+dcm_obj = datacursormode(gcf);
+set(dcm_obj,'DisplayStyle','datatip','SnapToDataVertex','off','Enable','on');
+set(dcm_obj,'UpdateFcn',{@dcmcallback,stationName});
